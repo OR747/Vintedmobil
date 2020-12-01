8308fc6c0d2443535097cc92db6f90a9f907c578
RHARRABT <rharrabt@MacBook-Pro-de-RHARRABT.local>
2020-12-01 15:05:43 +0100

    V10

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -4,6 +4,7 @@ import {
   Button,
   Text,
   View,
+  FlatList,
   ActivityIndicator,
   StyleSheet,
 } from "react-native";
@@ -19,7 +20,7 @@ export default function HomeScreen() {
       const response = await axios.get(
         "https://lereacteur-vinted-api.herokuapp.com/offers"
       );
-      //console.log(response.data);
+      // console.log(response.data);
       setData(response.data);
       setIsLoading(false);
     } catch (error) {
@@ -35,14 +36,49 @@ export default function HomeScreen() {
     <ActivityIndicator size="large" color="#29b6be" />
   ) : (
     <View style={styles.container}>
-      <Text>Welcome to VINTED Home!</Text>
-      <Button
-        title="Go to Offer"
-        onPress={() => {
-          navigation.navigate("Offer", { userId: 123 });
+      <View>
+        <Text style={{ color: "black", fontSize: 20 }}>
+          {data.product_price}€
+        </Text>
+      </View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              style={styles.touchableOpacity}
+              onPress={() => {
+                navigation.navigate("Offer", {
+                  id: item._id,
+                });
+              }}
+            >
+              <View style={styles.container1}>
+                <View style={styles.image}>
+                  <Image
+                    source={{
+                      uri: item.product_image.secure_url,
+                    }}
+                    resizeMode="cover"
+                    style={styles.img}
+                  ></Image>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  img: {
+    height: 85,
+    width: 85,
+    // borderRadius: 50,
+    //borderWidth: 2,
+    // borderColor: "#FC8083",
+    // top: -30,
+  },
+});
